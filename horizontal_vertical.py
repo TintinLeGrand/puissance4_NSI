65ffa3f853916d2e829b0a7d072b759573b70a03
Dounet17 <76966081+Dounet17@users.noreply.github.com>
2021-01-04 19:18:12 +0100

    Add files via upload

--- horizontal_vertical.py
+++ horizontal_vertical.py
@@ -12,11 +12,35 @@
 def alignement_horizontal(grille):
     ''' Cette fonction renvoie True si 4 pions de même couleur sont alignés
         horizontalement. Elle renvoie False sinon.   '''
+    c = 0
+    l = 5
+    compt1 = 0
+    compt = 0
+    def ligne():
+        while compt1 <= 4 :
+            if grille[l][c] == 0 :
+                
+            if grille[l][c] == grille[l][c+1] == grille[l][c+2] == grille[l][c+3]:
+                compt1 = 5
+                return True
+            else :
+                compt1 = compt1 + 1
+                c = c + 1
+                
+                
+    while compt <= 6 :
+        if ligne() == True:
+            compt = 7
+            return True
+        else :
+            compt = compt + 1
+            l = l - 1
+    
     pass     # permet d'exécuter une fonction vide
 
 def alignement_vertical(grille):
     ''' Cette fonction renvoie True si 4 pions de même couleur sont alignés
-        horizontalement. Elle renvoie False sinon.   '''
+        verticalement. Elle renvoie False sinon.   '''
     pass     # permet d'exécuter une fonction vide
 
 def alignement_quadrillage(grille):
