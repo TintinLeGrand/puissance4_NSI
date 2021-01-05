9f29cc7e5a132a0fc98330f1543dcb970d3489d0
Dounet17 <76966081+Dounet17@users.noreply.github.com>
2021-01-05 11:38:20 +0100

    Add files via upload

--- horizontal_vertical.py
+++ horizontal_vertical.py
@@ -14,11 +14,12 @@ def alignement_horizontal(grille):
         horizontalement. Elle renvoie False sinon.   '''
     c = 0
     l = 5
-    compt1 = 0
     compt = 0
+    compt1 = 0
     def ligne():
-        while compt1 <= 4 :
+        while compt1 < 4 :
             if grille[l][c] == 0 :
+                compt1 = 5
                 
             if grille[l][c] == grille[l][c+1] == grille[l][c+2] == grille[l][c+3]:
                 compt1 = 5
@@ -35,16 +36,47 @@ def alignement_horizontal(grille):
         else :
             compt = compt + 1
             l = l - 1
+            c = 0
     
     pass     # permet d'exécuter une fonction vide
 
 def alignement_vertical(grille):
     ''' Cette fonction renvoie True si 4 pions de même couleur sont alignés
         verticalement. Elle renvoie False sinon.   '''
+    c = 0
+    l = 0
+    compt = 0
+    compt1 = 0
+    def ligne():
+        while compt1 < 4 :
+            if grille[l][c] == 0 :
+                compt1 = 5
+                
+            if grille[l][c] == grille[l+1][c] == grille[l+2][c] == grille[l+3][c]:
+                compt1 = 5
+                return True
+            else :
+                compt1 = compt1 + 1
+                l = l + 1
+                
+                
+    while compt <= 6 :
+        if ligne() == True:
+            compt = 7
+            return True
+        else :
+            compt = compt + 1
+            c = c + 1
+            l = 0
     pass     # permet d'exécuter une fonction vide
 
 def alignement_quadrillage(grille):
     ''' Cette fonction renvoie True si la grille contient un alignement
         horizontal ou vertical de 4 pions de même couleur.
         Sinon elle renvoie False. '''
+    
+    if alignement_horizontal(grille) or alignement_vertical(grille) == True:
+        return True
+    else :
+        return False
     pass     # permet d'exécuter une fonction vide
